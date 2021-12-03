6cb2f80ad685c1ef21e4d664f2033d4b1a478f24
Md Saif Ullah <mdsaifullah.wd@gmail.com>
2021-12-04 02:26:57 +0600

    add an text input if other option selected in vahicle type

--- js/main.js
+++ js/main.js
@@ -1,12 +1,33 @@
+// Owner Info Show & Hide
 function show() {
     document.getElementById("ownerInfo").style.display = "grid";
 }
 function hide() {
     document.getElementById("ownerInfo").style.display = "none";
 }
+
+// Commission input Show & Hide
 function showCommission() {
     document.getElementById("commission").style.display = "block";
 }
 function hideCommission() {
     document.getElementById("commission").style.display = "none";
 }
+
+// Other Vahicle Type Show & Hide
+function showOtherVahicleType() {
+    document.getElementById("otherVahicleType").style.display = "block";
+}
+function hideOtherVahicleType() {
+    document.getElementById("otherVahicleType").style.display = "none";
+}
+
+
+$('.select').change(function(){
+     if($('.select option:selected').text() == "অন্যান্য"){
+        $('#otherVahicleType').show();
+     }
+     else{
+        $('#otherVahicleType').hide();
+     }
+ });
